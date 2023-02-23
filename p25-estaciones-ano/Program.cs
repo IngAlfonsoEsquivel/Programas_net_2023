468b7de9b023a15d077c65c439b307fc70a0a581
IngAlfonsoEsquivel <32135860@uaz.edu.mx>
2023-02-23 09:47:07 -0800

    23 F

---
+++ p25-estaciones-ano/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Muestra la estacion del ano que corresponde al numero 1,2,3,4 ");
+Console.Write("Dame un numero entre 1 y 4?      ");
+int n = int.Parse(Console.ReadLine());
+if(n>0 && n<5 ){
+    if(n==1) Console.WriteLine("Primavera\n");
+    if(n==2) Console.WriteLine("Verano\n");
+    if(n==3) Console.WriteLine("Otoño\n");
+    if(n==4) Console.WriteLine("Invierno\n");
+
+}
+else{
+    Console.WriteLine("En que planeta vives");
+}
+
---
+++ p26--tipo-angulo/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Muestra el tipo de angulo (0..360)\n");
+Console.Write("Dame un angulo ? ");
+int angulo = int.Parse(Console.ReadLine());
+if(angulo <0 || angulo >360){
+    Console.Write("Pongase Listo mijo ");
+}
+else{
+    if(angulo==0)
+    Console.WriteLine("\nEs Nulo");
+    if(angulo!=0 && angulo<90)
+    Console.WriteLine("\nEs agudo");
+    if(angulo==90)
+    Console.WriteLine("\nEs recto");
+    if(angulo>90 && angulo<180)
+    Console.WriteLine("\nEs obtuso");
+    if(angulo==180)
+    Console.WriteLine("\nEs llano");
+    if(angulo>180 && angulo<360)
+    Console.WriteLine("\nEs concavo");
+    if(angulo==360)
+    Console.WriteLine("\nEs completo");
+}
